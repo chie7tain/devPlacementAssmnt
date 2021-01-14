55de1c3b9dcfd17d2fffe3701a6e7fde828b297c
Ifeanyi Fredrick Okwuobi <fredrickokwuobi@gmail.com>
2021-01-14 17:34:34 +0100

    pagination complete wheew

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // dom elements
-const displayUserData = [...document.querySelectorAll(".expand-user-details-btn")];
+
 const userCardView = document.querySelector(".user-card-view");
 const userCardContainer = document.querySelector(".user-card-container");
 const filterBtns = document.querySelectorAll(".filter-btn")
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 // creating a user class
-class Users{
-  async getUsersData(){
+// class Users{
+  async function getUsersData(){
     try{
      let results = await fetch("https://randomuser.me/api/?page=6&results=25&seed=abcdefgh&inc=gender,name,email,phone,cell,picture,location,nat&noinfo");
      return results;
@@ -15,93 +15,94 @@ class Users{
       console.log(error);
     }
   }
-}
-class UI{
-  paginateUserData(data){
-    let page = 0;
-    let paginatedUserData = [];
-    function initialPagination(arr){
-      arr = [];
+// }
+let page = 0;
+let paginatedUserData = [];
+function paginateUserDataInitial(data){
       for(let i = 0; i < page + 5; i++){
-        arr.push(data[i]);
+        paginatedUserData.push(data[i]);
       }
-      return arr;
-    }
-    paginatedUserData = initialPagination(paginatedUserData);
-    function subsequentPagination(arr){
-      console.log("in nextBtn")
+    displayUsersData(paginatedUserData);
+        nextBtn.addEventListener("click",()=>{
       if(page == data.length - 5){
         page = 0;
       }else{
         page += 5;
       }
-      arr = [];
-      for(let i = 0; i < page + 5; i++){
-        arr.push(data[i]);
+      paginatedUserData = [];
+      for(let i = page; i < page + 5; i++){
+        paginatedUserData.push(data[i])
       }
-      console.log(arr);
-      return arr;
-    }
-    nextBtn.addEventListener("click",()=>{
-      console.log(paginatedUserData)
-     paginatedUserData = subsequentPagination(paginatedUserData);
-      console.log(paginatedUserData)
+      displayUsersData(paginatedUserData)
     })
-    console.log(paginatedUserData)
-    return paginatedUserData;
+
+    prevBtn.addEventListener("click",()=>{
+      if(page == 0){
+        page = data.length - 5;
+      }else{
+        page -= 5;
+      }
+      paginatedUserData = [];
+      for(let i = page; i < page + 5; i++){
+        paginatedUserData.push(data[i]);
+      }
+      displayUsersData(paginatedUserData);
+    })
   }
-  displayUsersData(usersData){
-    usersData = this.paginateUserData(usersData);
-    let personData = usersData.map(userData =>{
-      return `
-       <div class="individual-user-card-container">
-              <!-- user-image -->
-              <div class="user-image-container">
-                <div class="image-border">
-                  <img src="${userData.picture.medium}" alt="user's image" class="user-image">
-                </div>
-              </div>
-              <!-- user details -->
-              <div class="user-details-container">
-                <!-- user name -->
-                <div class="user-name-container">
-                  <h3 class="user-name">${userData.name.first} ${userData.name.last}</h3>
-                </div>
-                <!-- user address -->
-                <div class="user-address-container">
-                  <p class="user-address">
-                    ${userData.location.street.number} ${userData.location.street.name} ${userData.location.city} ${userData.location.state} ${userData.location.country}
-                  </p>
-                </div>
-                <!-- user contact details -->
-                <div class="user-contact-details-container">
-                  <div class="user-email-container">
-                    <p class="user-email-address">
-                      <i class="far fa-envelope"></i>${userData.email}
-                    </p>
-                  </div>
-                  <div class="user-phone-number-container">
-                    <p class="user-phone-number">
-                      <i class="fas fa-phone-alt"></i>
-                      ${userData.phone}
-                    </p>
+
+// class UI{
+
+  function displayUsersData(usersData){
+      let personData = usersData.map(userData =>{
+        return `
+         <div class="individual-user-card-container">
+                <!-- user-image -->
+                <div class="user-image-container">
+                  <div class="image-border">
+                    <img src="${userData.picture.medium}" alt="user's image" class="user-image">
                   </div>
                 </div>
+                <!-- user details -->
+                <div class="user-details-container">
+                  <!-- user name -->
+                  <div class="user-name-container">
+                    <h3 class="user-name">${userData.name.first} ${userData.name.last}</h3>
+                  </div>
+                  <!-- user address -->
+                  <div class="user-address-container">
+                    <p class="user-address">
+                      ${userData.location.street.number} ${userData.location.street.name} ${userData.location.city} ${userData.location.state} ${userData.location.country}
+                    </p>
+                  </div>
+                  <!-- user contact details -->
+                  <div class="user-contact-details-container">
+                    <div class="user-email-container">
+                      <p class="user-email-address">
+                        <i class="far fa-envelope"></i>${userData.email}
+                      </p>
+                    </div>
+                    <div class="user-phone-number-container">
+                      <p class="user-phone-number">
+                        <i class="fas fa-phone-alt"></i>
+                        ${userData.phone}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <!-- expand user details button -->
+                <div class="expand-user-details-button-container">
+                  <button class="expand-user-details-btn">
+                  <i class="fas fa-arrow-right"></i>
+                  </button>
+                </div>
               </div>
-              <!-- expand user details button -->
-              <div class="expand-user-details-button-container">
-                <button class="expand-user-details-btn">
-                <i class="fas fa-arrow-right"></i>
-                </button>
-              </div>
-            </div>
-      `;
-    })
+        `;
+      })
     personData = personData.join("");
     userCardContainer.innerHTML = personData;
   }
   // this function would help to filter out users based on gender using the filter buttons
-  filterUsers(userData){
+  function filterUsers(userData){
     // filter users
     filterBtns.forEach(btn =>{
       btn.addEventListener("click",(e)=>{
@@ -113,26 +114,26 @@ class UI{
           }
         });
         if(category === "all"){
-          this.displayUsersData(userData);
+          displayUsersData(userData);
         }else{
-          this.displayUsersData(genderCategory);
+          displayUsersData(genderCategory);
         }
       })
     })
   }
-}
+// }
 window.addEventListener("DOMContentLoaded",()=>{
 // creating new instances of the users and  UI class
-    const users = new Users();
-    const display = new UI();
-    users.getUsersData()
+    // const users = new Users();
+    // const display = new UI();
+    getUsersData()
     .then(response => response.json())
     .then(data =>{
-      // destructuring the data into results
       let {results} = data;
-      display.displayUsersData(results);
-      display.filterUsers(results);
-      display.paginateUserData(results);
+      displayUsersData(results);
+      filterUsers(results);
+      paginateUserDataInitial(results);
+
     })
 })
 

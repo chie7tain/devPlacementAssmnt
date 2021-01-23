9a0eb88f0269001777ad16c923efd6b61fe78302
Ifeanyi Fredrick Okwuobi <fredrickokwuobi@gmail.com>
2021-01-23 18:14:25 +0100

    add display individual user profile

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // dom elements
 const apiUrl = 'https://randomuser.me/api/?results=25&seed=abc&inc=gender,name,registered,location,email,cell,phone,dob,picture,nat&noinfo'
-const userCardView = document.querySelector(".user-card-view");
+const userProfileDisplay = document.querySelector(".user-profile-display");
 const userCardContainer = document.querySelector(".user-card-container");
 const filterBtns = document.querySelectorAll(".filter-btn")
 const prevBtn = document.querySelector(".prev-btn");
@@ -16,10 +16,6 @@ class Users{
     try{
       let result = await fetch("./users.json");
       let users = await result.json();
-      // console.log(users)
-      // users = users.results;
-      // console.log(users.results)
-      // console.log(users)
       users = users.map(user =>{
         const {gender,email,phone,cell} = user;
         const {first:firstName,last:lastName} = user.name;
@@ -92,26 +88,22 @@ class UI{
               </div>
         `;
       });
-      userCardDisplay.innerHTML = result;
+      userCardDisplay.innerHTML = result
     }
 
-    getUserDetailsBtn(){
-
-      const btns = [...document.querySelectorAll(".expand-user-details-btn")];
-
-                let backBtn = document.querySelector(".back-btn-container");
-                console.log(backBtn)
-      btns.forEach(btn =>{
+    getUserDetailsBtn(users){
+      const expandDetailsBtns= [...document.querySelectorAll(".expand-user-details-btn")];
+      expandDetailsBtns.forEach(btn =>{
         let email = btn.dataset.email;
         btn.addEventListener("click",()=>{
           let user = Storage.getUser(email);
-          // hide user card container
+       // hide user card container
           this.hideUserCardContent();
+          // display full profile
           this.displayFullProfile(user);
           this.showUserProfile();
           // this.showBackBtn();
           this.returnToResults()
-          // display full profile
         })
       })
     }
@@ -162,7 +154,7 @@ class UI{
                 </div>
 
       `;
-      userCardView.innerHTML = userProfile;
+      userProfileDisplay.innerHTML = userProfile;
     }
     paginateUserData(data){
         let page = 0;
@@ -176,6 +168,7 @@ class UI{
         this.displayUsersData(paginatedUserData);
         dataForFilter = paginatedUserData;
         this.filterUsers(dataForFilter)
+        this.getUserDetailsBtn(dataForFilter);
             nextBtn.addEventListener("click",()=>{
           if(page == data.length - usersPerPage){
             page = 0;
@@ -189,6 +182,7 @@ class UI{
         this.displayUsersData(paginatedUserData);
         dataForFilter = paginatedUserData;
           this.filterUsers(dataForFilter)
+          this.getUserDetailsBtn(dataForFilter);
         })
 
         prevBtn.addEventListener("click",()=>{
@@ -204,7 +198,7 @@ class UI{
           this.displayUsersData(paginatedUserData);
           dataForFilter = paginatedUserData;
           this.filterUsers(dataForFilter)
-
+          this.getUserDetailsBtn(dataForFilter);
         })
 
     }
@@ -222,8 +216,10 @@ class UI{
         });
         if(category === "all"){
           this.displayUsersData(userData);
+          this.getUserDetailsBtn(userData)
         }else{
           this.displayUsersData(genderCategory);
+          this.getUserDetailsBtn(genderCategory);
         }
       })
     })
@@ -234,12 +230,17 @@ class UI{
     hideUserCardContent(){
     userCardDisplay.classList.add("hide-user-card")
     }
+    // this function helps the user view the full user profile when he/she clicks the expand user details btn
     showUserProfile(){
-      userCardView.classList.add("show-user-card-view");
+      userProfileDisplay.classList.add("show-user-profile");
+      backBtn.classList.add("show-back-btn");
     }
+    // this function helps to return the user back to the previous page where he/she clicked from
     returnToResults(){
       backBtn.addEventListener("click",()=>{
          userCardDisplay.classList.remove("hide-user-card")
+         userProfileDisplay.classList.remove("show-user-profile");
+         backBtn.classList.remove("show-back-btn");
       })
     }
   }
@@ -258,107 +259,17 @@ class Storage{
 
 // class UI{
 
-  function displayUsersData(usersData){
 
-      let personData = usersData.map(userData =>{
-        return `
-         <div class="individual-user-card-container">
-                <!-- user-image -->
-                <div class="user-image-container">
-                  <div class="image-border">
-                    <img src="${userData.picture.thumbnail}" alt="user's image" class="user-image">
-                  </div>
-                </div>
-                <!-- user details -->
-                <div class="user-details-container">
-                  <!-- user name -->
-                  <div class="user-name-container">
-                    <h3 class="user-name">${userData.name.first} ${userData.name.last}</h3>
-                  </div>
-                  <!-- user address -->
-                  <div class="user-address-container">
-                    <p class="user-address">
-                      ${userData.location.street.number} ${userData.location.street.name} ${userData.location.city} ${userData.location.state} ${userData.location.country}
-                    </p>
-                  </div>
-                  <!-- user contact details -->
-                  <div class="user-contact-details-container">
-                    <div class="user-email-container">
-                      <p class="user-email-address">
-                        <i class="far fa-envelope"></i>${userData.email}
-                      </p>
-                    </div>
-                    <div class="user-phone-number-container">
-                      <p class="user-phone-number">
-                        <i class="fas fa-phone-alt"></i>
-                        ${userData.phone}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <!-- expand user details button -->
-                <div class="expand-user-details-button-container">
-                  <button class="expand-user-details-btn data-email="">
-                  <i class="fas fa-arrow-right"></i>
-                  </button>
-                </div>
-              </div>
-        `;
-      })
-    personData = personData.join("");
-    userCardContainer.innerHTML = personData;
-
-    const userCards = [...document.querySelectorAll(".individual-user-card-container")];
-
-    userCards.forEach(card =>{
-      const viewUserProfileBtn = card.querySelector(".expand-user-details-btn");
-      viewUserProfileBtn.addEventListener("click",()=>{
-        console.log(usersData)
-        userCardContainer.innerHTML = card;
-      })
-    })
-  }
-  // this function would help to filter out users based on gender using the filter buttons
-  function filterUsers(userData){
-    // filter users
-    filterBtns.forEach(btn =>{
-      btn.addEventListener("click",(e)=>{
-        const category = e.currentTarget.dataset.gender;
-        const genderCategory = userData.filter(userInfo =>{
-          // console.log(userInfo.gender);
-          if(userInfo.gender === category){
-            return userInfo;
-          }
-        });
-        if(category === "all"){
-          displayUsersData(userData);
-        }else{
-          displayUsersData(genderCategory);
-        }
-      })
-    })
-  }
-// }
 document.addEventListener("DOMContentLoaded",()=>{
 
   const ui = new UI();
   const users = new Users();
   // get all users
   users.getUsers().then(users => {
-    // ui.displayUsersData(users);
-    ui.paginateUserData(users);
+    ui.displayUsersData(users);
     Storage.saveUsers(users);
-  }).then(()=>{
-    ui.getUserDetailsBtn(users);
+    ui.paginateUserData(users)
   })
-    // getUsersData()
-    // .then(response => response.json())
-    // .then(data =>{
-    //   let {results} = data;
-    //   displayUsersData(results);
-    //   // filterUsers(results);
-    //  paginateUserDataInitial(results);
-    // })
 })
 
 
